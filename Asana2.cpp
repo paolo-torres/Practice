95001b68361eac9c19189b0e1eaa5552746d7025
paolotorres <paolo-torres@hotmail.com>
2019-05-28 20:00:12 -0400

    optimize loops

--- Asana2.cpp
+++ Asana2.cpp
@@ -1,63 +1,63 @@
 #include <iostream>
+#include <map>
 #include <tuple>
 #include <vector>
 
 using namespace std;
 
-int find_carrots(vector<vector<int>> garden, int row, int col) {
+int find_path(vector<vector<int>> garden, int row, int col) {
+	int cur_carrots = 0;
 	int max_carrots = 0;
-	int carrots = 0;
 	int new_row = 0;
 	int new_col = 0;
 
-	for (int i = -1; i < 1; i++) {
-		for (int j = -1; j < 1; j++) {
-			if (i == -1 && j == 0 || i == 1 && j == 0 || i == 0 && j == -1 || i == 0 && j == 1) {
-				if (row + i >= 0 && row + i < garden.size() &&
-					col + j >= 0 && col + j < garden[row].size()) {
-					if (garden[row + i][col + j] > max_carrots) {
-						max_carrots = garden[row + i][col + j];
-						new_row = row + i;
-						new_col = col + j;
-					}
-				}
+	multimap<int, int> m = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
+
+	for (auto it = m.begin(); it != m.end(); it++) {
+		if (row + it->first >= 0 && row + it->first < garden.size() &&
+			col + it->second >= 0 && col + it->second < garden[row].size()) {
+			if (garden[row + it->first][col + it->second] > max_carrots) {
+				max_carrots = garden[row + it->first][col + it->second];
+				new_row = row + it->first;
+				new_col = col + it->second;
 			}
 		}
 	}
 
-	carrots = garden[row][col];
+	cur_carrots = garden[row][col];
+
 	garden[row][col] = 0;
 
 	if (max_carrots > 0 && new_row >= 0 && new_row < garden.size() &&
 		new_col >= 0 && new_col < garden[row].size()) {
-		carrots += find_carrots(garden, new_row, new_col);
+		cur_carrots += find_path(garden, new_row, new_col);
 	}
 
-	return carrots;
+	return cur_carrots;
 }
 
 tuple<int, int> find_center(vector<vector<int>> garden) {
-	int row_options[] = { garden.size() / 2, garden.size() / 2};
-	int col_options[] = { garden[0].size() / 2, garden[0].size() / 2 };
+	int rows[] = { garden.size() / 2, garden.size() / 2 };
+	int cols[] = { garden[0].size() / 2, garden[0].size() / 2 };
 
 	if (garden.size() % 2 == 0) {
-		row_options[0]--;
+		rows[0]--;
 	}
 
 	if (garden[0].size() % 2 == 0) {
-		col_options[0]--;
+		cols[0]--;
 	}
 
 	int max_carrots = 0;
 	int max_row = 0;
 	int max_col = 0;
 
-	for (auto row_option : row_options) {
-		for (auto col_option : col_options) {
-			if (garden[row_option][col_option] > max_carrots) {
-				max_carrots = garden[row_option][col_option];
-				max_row = row_option;
-				max_col = col_option;
+	for (auto row : rows) {
+		for (auto col : cols) {
+			if (garden[row][col] > max_carrots) {
+				max_carrots = garden[row][col];
+				max_row = row;
+				max_col = col;
 			}
 		}
 	}
@@ -65,19 +65,18 @@ tuple<int, int> find_center(vector<vector<int>> garden) {
 	return { max_row, max_col };
 }
 
-int hungry_rabbit(vector<vector<int>> garden) {
+int find_carrots(vector<vector<int>> garden) {
 	if (garden.size() == 0 || garden[0].size() == 0) {
 		return 0;
 	}
 
-	vector<vector<int>> garden_copy = garden;
-	auto [row, col] = find_center(garden_copy);
+	auto [row, col] = find_center(garden);
 
 	if (!row || !col) {
 		return 0;
 	}
 
-	return find_carrots(garden_copy, row, col);
+	return find_path(garden, row, col);
 }
 
 int main()
@@ -88,7 +87,7 @@ int main()
 		{4, 6, 3, 4, 9},
 		{3, 1, 0, 5, 8} };
 
-	cout << "Carrots Eaten: " << hungry_rabbit(garden);
+	cout << "Carrots Eaten: " << find_carrots(garden) << '\n';
 
 	return 0;
 }
