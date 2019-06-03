71516f088249256f5a73e2a1a4420796fdcd8a58
paolotorres <paolo-torres@hotmail.com>
2019-06-02 22:51:14 -0400

    add bst practice

--- bst.cpp
+++ bst.cpp
@@ -1,13 +1,20 @@
+#include <algorithm>
 #include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+// Binary Search Tree
 
 struct Node {
 	int data;
 	struct Node *left, *right;
 };
 
-struct Node* newNode(int value) {
+struct Node* newNode(int dataue) {
 	struct Node* temp = new Node;
-	temp->data = value;
+	temp->data = dataue;
 	temp->left = NULL;
 	temp->right = NULL;
 	return temp;
@@ -36,7 +43,7 @@ struct Node* insert(struct Node* node, int data) {
 	return node;
 }
 
-struct Node* minValueNode(struct Node* node) {
+struct Node* mindataueNode(struct Node* node) {
 	struct Node* curr = node;
 
 	while (curr && curr->left != NULL) {
@@ -69,12 +76,194 @@ struct Node* deleteNode(struct Node* root, int data) {
 			return temp;
 		}
 
-		struct Node* temp = minValueNode(root->right);
+		struct Node* temp = mindataueNode(root->right);
 		root->data = temp->data;
 		root->right = deleteNode(root->right, temp->data);
 	}
 
 	return root;
+}
+
+// Find Mode
+
+void traverse(Node* root, map<int, int>& m, vector<int>& max) {
+	if (root != NULL) {
+		traverse(root->left, m, max);
+		auto find = m.find(root->data);
+		if (find != m.end()) {
+			find->second++;
+			if (max[1] < find->second) {
+				max[0] = find->first;
+				max[1] = find->second;
+			}
+		}
+		else {
+			m.insert({ root->data, 1 });
+		}
+		traverse(root->right, m, max);
+	}
+}
+
+vector<int> findMode(Node* root) {
+	map<int, int> m;
+	vector<int> max = { 0, 0 };
+
+	traverse(root, m, max);
+
+	return max;
+}
+
+// Lowest Common Ancestor
+
+Node* findLCA(Node* root, Node* p, Node* q) {
+	if (p->data < root->data && q->data < root->data) {
+		return findLCA(root->left, p, q);
+	}
+	else if (p->data > root->data && q->data > root->data) {
+		return findLCA(root->right, p, q);
+	}
+	else {
+		return root;
+	}
+}
+
+Node* lowestCommonAncestor(Node* root, Node* p, Node* q) {
+	return findLCA(root, p, q);
+}
+
+// Search in a Binary Search Tree
+
+Node* searchBST(Node* root, int val) {
+	if (root == NULL) {
+		return NULL;
+	}
+
+	if (val < root->data) {
+		return searchBST(root->left, val);
+	}
+	else if (val > root->data) {
+		return searchBST(root->right, val);
+	}
+	else {
+		return root;
+	}
+}
+
+// Convert Sorted Array to Binary Search Tree
+
+Node* helper(vector<int> &nums, int low, int high) {
+	if (low > high) {
+		return NULL;
+	}
+
+	int mid = (low + high) / 2;
+
+	Node* node = new Node;
+	node->data = nums[mid];
+
+	node->left = helper(nums, low, mid - 1);
+	node->right = helper(nums, mid + 1, high);
+
+	return node;
+}
+
+Node* sortedArrayToBST(vector<int>& nums) {
+	if (nums.size() == 0) {
+		return NULL;
+	}
+
+	Node* head = helper(nums, 0, nums.size() - 1);
+
+	return head;
+}
+
+// Binary Search
+
+int helper_2(vector<int>& nums, int target, int low, int high) {
+	if (low > high) {
+		return -1;
+	}
+
+	int mid = (low + high) / 2;
+
+	if (target < nums[mid]) {
+		return helper_2(nums, target, low, mid - 1);
+	}
+	else if (target > nums[mid]) {
+		return helper_2(nums, target, mid + 1, high);
+	}
+	else {
+		return nums[mid];
+	}
+}
+
+int search(vector<int>& nums, int target) {
+	int low = 0;
+	int high = nums.size() - 1;
+
+	return helper_2(nums, target, low, high);
+}
+
+// Univalued Binary Tree
+
+bool isUnivalTree(Node* root) {
+	int val = root->data;
+	if (root != NULL) {
+		isUnivalTree(root->left);
+		if (root->data != val) {
+			return false;
+		}
+		isUnivalTree(root->right);
+	}
+
+	return true;
+}
+
+// Invert Binary Tree
+
+Node* invertTree(Node* root) {
+	if (root == NULL) {
+		return NULL;
+	}
+
+	Node* left = invertTree(root->left);
+	Node* right = invertTree(root->right);
+
+	root->left = right;
+	root->right = left;
+
+	return root;
+}
+
+// Maximum Depth of Binary Tree
+
+int maxDepth(Node* root) {
+	if (root == NULL) {
+		return 0;
+	}
+
+	return 1 + max(maxDepth(root->left), maxDepth(root->right));
+}
+
+// Balanced Binary Tree
+
+int depth(Node* root) {
+	if (root == NULL) {
+		return 0;
+	}
+
+	return max(depth(root->left), depth(root->right)) + 1;
+}
+
+bool isBalanced(Node* root) {
+	if (root == NULL) {
+		return true;
+	}
+
+	int left = depth(root->left);
+	int right = depth(root->right);
+
+	return abs(left - right) <= 1 && isBalanced(root->left) && isBalanced(root->right);
 }
 
 int main()
